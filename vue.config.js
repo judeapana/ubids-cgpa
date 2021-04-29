49c0d3c2a58c806d572e0fab9b3e32c333169fbf
Jude Apana <0554apana@gmail.com>
2021-04-29 18:47:24 +0000

    dep

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    transpileDependencies: [
-        'vuetify'
-    ],
-    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+  transpileDependencies: [
+    'vuetify'
+  ],
+  pwa: {
+    name: 'C/GPA Calculator',
+  }
 }
